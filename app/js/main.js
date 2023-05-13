0ee0d34bc85f38229fd54554501dfbe1f952ed02
Karina <karinadesyatnik27@gmail.com>
2023-05-13 11:37:08 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,18 @@ $('.menu-link, .close').click(function(){
     $('.menu-content-wrapper').removeClass('active');
 })
 });
+
+$(".smooth").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < 100) {
+      $(".top").fadeOut();
+    } else {
+      $(".top").fadeIn();
+    }
+  });
